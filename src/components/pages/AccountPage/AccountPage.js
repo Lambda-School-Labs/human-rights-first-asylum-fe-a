9e236a14190798c4761ddcd3bacb9530a4c0e035
bukit3point0 <bukit3point0@gmail.com>
2021-07-14 17:30:30 -0400

    account page can now add a profile picture

--- src/components/pages/AccountPage/AccountPage.js
+++ src/components/pages/AccountPage/AccountPage.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Button, Card, Avatar, Modal, Form, Input } from 'antd';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
+import ImgCrop from 'antd-img-crop';
+import { Upload } from 'antd';
 
 import './_AccountPageStyles.less';
 import Icon from '@ant-design/icons';
@@ -10,6 +12,7 @@ const initialFormValues = {
   first_name: '',
   last_name: '',
   email: '',
+  avatarUrl: '',
 };
 
 const AccountPage = props => {
@@ -27,8 +30,8 @@ const AccountPage = props => {
       .get(`/profile/${id}`)
       .then(res => {
         console.log(res.data);
-        const { first_name, last_name, email, user_id } = res.data;
-        setFormValues({ first_name, last_name, email, user_id });
+        const { first_name, last_name, email, user_id, avatarUrl } = res.data;
+        setFormValues({ first_name, last_name, email, user_id, avatarUrl });
         setIsEditModalVisible(true);
       });
   };
@@ -58,6 +61,7 @@ const AccountPage = props => {
       last_name: formValues.last_name.trim(),
       email: formValues.email.trim(),
       user_id: formValues.user_id.trim(),
+      avatarUrl: formValues.avatarUrl,
     };
     updateUser(updatedUser);
     setIsEditModalVisible(false);
@@ -168,6 +172,9 @@ const AccountPage = props => {
                     className="text-field"
                     value={formValues.email}
                   />
+                </Form.Item>
+                <Form.Item label="avatarUrl">
+                  <Input />
                 </Form.Item>
               </Form>
             </Modal>
